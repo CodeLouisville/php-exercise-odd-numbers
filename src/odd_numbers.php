dc6f5c0af345679dc0d67fc1d772f05db3634e78
Korey McMurtrey <kmcmurtrey@gmail.com>
2016-01-27 21:55:03 -0500

    Adds is_int() and type hinting

--- src/odd_numbers.php
+++ src/odd_numbers.php
@@ -1,11 +1,11 @@
 <?php
 
-function odd_numbers($array) {
+function odd_numbers(array $arrayInput) {
     $odd = array();
 
-    if (isset($array) && is_array($array)) {
-        foreach ($array as $value) {
-            if ($value % 2 !== 0) {
+    if (isset($arrayInput) && is_array($arrayInput)) {
+        foreach ($arrayInput as $value) {
+            if ($value % 2 !== 0 && is_int($value) === true) {
                 $odd[] = $value;
             }
         }
