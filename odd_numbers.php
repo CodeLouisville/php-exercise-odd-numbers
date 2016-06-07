378cc337941b98984b844e68ee78a755b97ed6b5
Tony T <TonyTatsumaki@live.com>
2016-06-07 18:34:53 -0400

    Odd Numbers Exercise
    Because we just can't even?

---
+++ odd_numbers.php
@@ -0,0 +1,30 @@
+<?php
+
+function odd_numbers($input){
+  $array = array_filter($input, "number");
+  $oddNumber = array_filter($array, "odd");
+  $win = sort($oddNumber);
+
+  return $oddNumber;
+}
+
+//check if it's a number!
+function number($n){
+    if (is_int($n)){
+      return true;
+    } else {
+      return false;
+    }
+  } 
+
+// returns whether the integer is odd
+function odd($var){
+    if (abs($var % 2)){
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+?>
